972634598d88905e579aabca1ca456e5a9852b7e
giodimagio <giodimagio@gmail.com>
2023-05-28 00:14:19 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uah-aplicaciones-moviles-hibridas-trabajoInicial',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
