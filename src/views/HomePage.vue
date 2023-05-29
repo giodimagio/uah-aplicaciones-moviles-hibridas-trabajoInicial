70f87857ec3a19d85e3535cf12b1eb6ad2dee339
giodimagio <giodimagio@gmail.com>
2023-05-29 21:05:30 +0200

    [HomePage] Diseño responsive de la interfaz gráfica según requisitos

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,23 +12,130 @@
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" offset="3">
+            <h1 style="text-align: center;">Album example</h1>
+          </ion-col>
+          <ion-col size="6" offset="3">
+            <h3 style="text-align: center;">Something short and leading about the collection below—its contents, the
+              creator, etc. Make it short and sweet,
+              but not too short so folks don't simply skip over it entirely.</h3>
+          </ion-col>
+        </ion-row>
+        <ion-grid>
+          <ion-row class="ion-justify-content-center">
+            <ion-col size="auto">
+              <ion-button fill="outline">Main call to action</ion-button>
+            </ion-col>
+            <ion-col size="auto">
+              <ion-button fill="outline">Secondary action</ion-button>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+        <ion-row>
+          <ion-col size-xs=12 size-sm=6 size-lg=4>
+            <ion-card>
+              <img alt="Thumbnail" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+              <ion-card-header>
+              </ion-card-header>
+              <ion-card-content>
+                This is a wider card with supporting text below as a natural lead-in to additional content.
+                This content is a little bit longer.
+              </ion-card-content>
+              <ion-grid>
+                <ion-row>
+                  <ion-col>
+                    <ion-button fill="outline">View</ion-button>
+                    <ion-button fill="outline">Edit</ion-button>
+                  </ion-col>
+                  <ion-col>
+                    <p style="text-align: end;">9 min</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card>
+          </ion-col>
+          <ion-col size-xs=12 size-sm=6 size-lg=4>
+            <ion-card>
+              <img alt="Thumbnail" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+              <ion-card-header>
+              </ion-card-header>
+              <ion-card-content>
+                This is a wider card with supporting text below as a natural lead-in to additional content.
+                This content is a little bit longer.
+              </ion-card-content>
+              <ion-grid>
+                <ion-row>
+                  <ion-col>
+                    <ion-button fill="outline">View</ion-button>
+                    <ion-button fill="outline">Edit</ion-button>
+                  </ion-col>
+                  <ion-col>
+                    <p class="container">9 min</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card>
+          </ion-col>
+          <ion-col size-xs=12 size-sm=6 size-lg=4>
+            <ion-card>
+              <img alt="Thumbnail" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+              <ion-card-header>
+              </ion-card-header>
+              <ion-card-content>
+                This is a wider card with supporting text below as a natural lead-in to additional content.
+                This content is a little bit longer.
+              </ion-card-content>
+              <ion-grid>
+                <ion-row>
+                  <ion-col>
+                    <ion-button fill="outline">View</ion-button>
+                    <ion-button fill="outline">Edit</ion-button>
+                  </ion-col>
+                  <ion-col>
+                    <p>9 min</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
     </ion-content>
+
+    <ion-footer>
+      <ion-toolbar>
+        <ion-title>Footer</ion-title>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonFooter } from '@ionic/vue';
+import { IonGrid, IonCol, IonRow } from '@ionic/vue';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 </script>
 
 <style scoped>
+h1,
+h3,
+p {
+  margin: 0;
+  padding: 10px;
+  color: #8c8c8c;
+}
+
+p {
+  font-size: 14px;
+  text-align: end;
+}
+
 #container {
   text-align: center;
-  
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +151,7 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
   color: #8c8c8c;
-  
   margin: 0;
 }
 
