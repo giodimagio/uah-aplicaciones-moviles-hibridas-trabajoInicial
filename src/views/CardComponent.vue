edaec2de587ad13fede0215338e768e6c0e5c1bd
giodimagio <giodimagio@gmail.com>
2023-06-12 21:59:28 +0200

    [CardComponent] Comentarios explicativos

--- src/views/CardComponent.vue
+++ src/views/CardComponent.vue
@@ -1,3 +1,5 @@
+<!-- Template: Componente HTML de tipo Card, de Ionic, que será utilizado en HomePage varias veces para representar una tarjeta responsiva. -->
+
 <template>
     <ion-card>
         <img alt="Thumbnail" src="https://ionicframework.com/docs/img/demos/card-media.png" />
@@ -19,10 +21,14 @@
     </ion-card>
 </template>
 
+<!-- Script: Elemento script (de tipo Composition API en Vue 3) que importa los componentes de Ionic necesarios. -->
+
 <script setup lang="ts">
-import { IonCard, IonCardContent, IonCardHeader } from '@ionic/vue';
+import { IonCard, IonCardContent } from '@ionic/vue';
 </script>
-  
+
+<!-- Style: Estilos CSS3 del Single-File Component que solo aplican a los párrafos este componente. -->
+
 <style scoped>
 p {
     margin: 0;
